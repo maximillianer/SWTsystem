3569d4959529b15434c561ea89bd06715632ff58
maximillian <maximilliaer@gmail.com>
2021-02-23 14:47:47 +0500

    Initial commit

---
+++ src/main/java/com/Eliseev/SWTsystem/SwTsystemApplication.java
@@ -0,0 +1,13 @@
+package com.Eliseev.SWTsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwTsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwTsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Eliseev/SWTsystem/SwTsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eliseev.SWTsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwTsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
